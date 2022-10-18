b1aa1a549a8fb25a094d8ebdc28ba5b64b306b26
PanBaron <artur.mykowski@gmail.com>
2022-10-18 12:02:28 +0200

    Add project files.

---
+++ MultiThreading.Task1.100Tasks/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * 1.	Write a program, which creates an array of 100 Tasks, runs them and waits all of them are not finished.
+ * Each Task should iterate from 1 to 1000 and print into the console the following string:
+ * “Task #0 – {iteration number}”.
+ */
+using System;
+using System.Threading.Tasks;
+
+namespace MultiThreading.Task1._100Tasks
+{
+    class Program
+    {
+        const int TaskAmount = 100;
+        const int MaxIterationsCount = 1000;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".Net Mentoring Program. Multi threading V1.");
+            Console.WriteLine("1.	Write a program, which creates an array of 100 Tasks, runs them and waits all of them are not finished.");
+            Console.WriteLine("Each Task should iterate from 1 to 1000 and print into the console the following string:");
+            Console.WriteLine("“Task #0 – {iteration number}”.");
+            Console.WriteLine();
+            
+            HundredTasks();
+
+            Console.ReadLine();
+        }
+
+        static void HundredTasks()
+        {
+            Task[] taskArray = new Task[TaskAmount];
+
+            for (int i = 0; i < taskArray.Length; i++)
+            {
+                int j = i;
+                taskArray[i] = new Task(() => Iterate(j));
+            }
+            foreach (Task task in taskArray)
+            {
+                task.Start();
+                task.Wait();
+            }
+
+        }
+
+        private static void Iterate(int taskNumber)
+        {
+            for (int i = 0; i < MaxIterationsCount; i++)
+            {
+                Output(taskNumber, i);
+            }
+        }
+
+        static void Output(int taskNumber, int iterationNumber)
+        {
+            Console.WriteLine($"Task #{taskNumber} – {iterationNumber}");
+        }
+    }
+}
